cf3156d3d5713efaaec107a934021b9a08485201
Divya Sharma <divyasharma1509ds@gmail.com>
2022-08-19 11:28:19 +0530

    add modifications to code for staircase search

--- 2DArrays/Course/sorted2DArraySearch.cpp
+++ 2DArrays/Course/sorted2DArraySearch.cpp
@@ -2,10 +2,16 @@
 //if we start searchin from either top right corner or bottom left corner moving in one direction will increase values whereas moving in othe direction will decrease values.
 //If the matrix(n x m) was only sorted say row wise, then we could apply binary search in each row and thus, the resulting time complexity: O(nlogm)
 //Time Complexity: O(n)
+//Also known as staircase search
 #include<iostream>
 using namespace std;
 
 pair<int, int> findElementSortedMatrix(int arr[][4], int rows, int cols, int key) {
+    //check if key lies within the range of the matrix
+    if(key<arr[0][0] or key > arr[rows-1][cols-1]){
+        make_pair(-1, -1);
+    }
+
     //starting from the upper right element
     int rowIdx = 0;
     int colIdx = cols - 1;

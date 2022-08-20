5869d93462f9f00a03e9038597762c824b27eac8
Divya Sharma <divyasharma1509ds@gmail.com>
2022-08-20 18:30:14 +0530

    add code for optimized version of counting num of set bits

--- Bit Manipulation/countBits.cpp
+++ Bit Manipulation/countBits.cpp
@@ -13,8 +13,18 @@ int countSetBits(int n) {
     return count;
 }
 
+//Time Complexity: O(number of set bits)
+int countSetBitsHack(int n) {
+    int count = 0;
+    while(n>0) {
+        n = n&(n-1);
+        count++;
+    }
+    return count;
+}
+
 int main() {
     int n = 15;
-    cout << countSetBits(n);
+    cout << countSetBits(n) << " " << countSetBitsHack(n);
     return 0;
 }

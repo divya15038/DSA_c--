458b710cbd37211e6995ce89a08b18127eae68db
Divya Sharma <divyasharma1509ds@gmail.com>
2022-08-21 10:24:58 +0530

    add code to find the last occurrence of element in the array

---
+++ Recursion/lastOccurence.cpp
@@ -0,0 +1,31 @@
+//Find the index of last occurence of an element in the given array using a recursive function.
+//Time complexity: O(n)
+//Space Cmplexity: O(n)
+#include<iostream>
+using namespace std;
+
+int lastOccurence(int arr[], int n, int key){
+    //base case
+    if(n == 0){
+        return -1;
+    }
+    //recursive case
+    if(arr[n-1] == key){
+        return n-1;
+    }
+    int SubIdx = lastOccurence(arr, n-1, key);
+    if(SubIdx==-1){
+        return -1;
+    }
+    return SubIdx;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 5, 2, 5};
+    int n = sizeof(arr)/sizeof(n);
+
+    int key; 
+    cin >> key;
+    cout << lastOccurence(arr, n, key);  
+    return 0;
+}

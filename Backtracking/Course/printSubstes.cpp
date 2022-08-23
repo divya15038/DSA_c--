2609591a9d23d79feee08bd7ddc65786f3dd76b9
Divya Sharma <divyasharma1509ds@gmail.com>
2022-08-23 20:21:44 +0530

    add changes to print all subsets code to print subsets sorted according to length

--- Backtracking/Course/printSubstes.cpp
+++ Backtracking/Course/printSubstes.cpp
@@ -2,28 +2,44 @@
 //Time Complexity: O(2^n)
 //Space Complexity: O(n)
 #include<bits/stdc++.h>
+#include<algorithm>
 using namespace std;
 
-void printSubsets(char *input, char *output, int i, int j){
+bool compare(string a, string b){
+    if(a.length() == b.length()){
+        return a < b;
+    }
+    return a.length() < b.length();
+}
+
+void printSubsets(char *input, char *output, int i, int j, vector<string> &v){
     //base case 
     if(input[i] == '\0'){
         output[j] = '\0';
-        cout << output << endl;
+        string temp(output);
+        v.push_back(temp);
         return;
     }
     //recursive case
     //include the ith character in output array
     output[j] = input[i];
-    printSubsets(input, output, i+1, j+1);
+    printSubsets(input, output, i+1, j+1, v);
     //don't include the ith character in input array
-    printSubsets(input, output, i+1, j);
+    printSubsets(input, output, i+1, j, v);
 }
 
 int main() {
     char input[100], output[100];
     cin >> input;
 
-    
-    printSubsets(input, output, 0, 0);
+    vector<string> v;
+    printSubsets(input, output, 0, 0, v);
+
+    //sort according to lenght and then lexicographically
+    sort(v.begin(), v.end(), compare);
+
+    for(auto x: v){
+        cout << x << endl;
+    }
     return 0;
 }
